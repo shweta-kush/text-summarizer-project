558fd99895c393c9726ed7e1f4d9803a8a22d91a
Shweta-kush <kushishweta@gmail.com>
2025-06-27 19:47:52 +0530

    template-creation

---
+++ template.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+Project_name ="TextSummarizer"
+list_of_files= {
+    ".github.com/workflows/.gitkeep",
+    "src/project_name/__init__.py",
+    "src/project_name/components/__init__.py",
+    "src/project_name/utils/__init__.py",
+    "src/project_name/utils/common.py",
+    "src/project_name/logging/__init__.py",
+    "src/project_name/config/__init__.py",
+    "src/project_name/config/configuration.py",
+    "src/project_name/pipline/__init__.py",
+    "src/project_name/entity/__init__.py",
+    "src/project_name/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "requirements.txt",
+    "Dockerfile",
+    "README.md",
+    "setup.py",
+    "research/trials.py",
+    # "tests/test_project_name.py"
+
+}
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            # file.write("# " + filename)
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File: {filepath} already exists.")
+
+
+
+
